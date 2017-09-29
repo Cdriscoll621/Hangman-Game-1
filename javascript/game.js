3b5c3d98eb5cc0df966223809760a23d4714d8fc
RaptorCentauri <dave.leo.shilander@icloud.com>
2017-09-28 22:57:56 -0500

    Site Files

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,5 @@
 //Make an array of words
-var wordList = ["Stargate", "Atlantis", "Ancients", "Asgard", "Nox", "ZPM"];
+var wordList = ["Stargate", "Atlantis", "Ancients", "Asgard", "Nox", "ZPM", "Thor", "Ra"];
 
 //Pick a random word from the array
 
@@ -27,35 +27,49 @@ var wordList = ["Stargate", "Atlantis", "Ancients", "Asgard", "Nox", "ZPM"];
 // 	new_word.innerHTML = wordList[i];
 // 	// console.log(wordList[i]);
 // 	tempList.appendChild(new_word);
+// this.substr(index+replacement.length
 // }
 //===============================================
 
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, parseInt(index)) + replacement + this.substr(parseInt(index)+1);
+}
 
 
-//Pick a random word from the array =====================================================
-randomNum = Math.floor(Math.random()*wordList.length);
-randomWord = wordList[randomNum].toLowerCase();
+//Pick a random word from the array  and make an new array from that word=====================================================
+var randomNum = Math.floor(Math.random()*wordList.length);
+var randomWord = wordList[randomNum].toLowerCase();
+var hiddenWord = randomWord.replace(/\w/gi, "#");
+var randomWord = wordList[randomNum].toLowerCase().split("");
 console.log("Random Word is: " + randomWord);
 // var wordLength = wordList[randomNum].length;
 console.log(randomWord + " has " + randomWord.length + " letters.")
+var updateWord = hiddenWord;
+
+
+//convert randomWord to and object
+var wordObject = Object.assign({},randomWord);
+console.log(wordObject);
 //======================================================================================
+
 
 
 // Display the random word and replace each character with "_" =========================
 var tempList = document.getElementById("list");
-var hiddenWord = randomWord.replace(/\w/gi, "#");
+// var hiddenWord = randomWord.replace(/\w/gi, "#");
 console.log("The hidden version looks like this: " + hiddenWord);
 var hidden = document.createElement("p");
+var random = document.createElement("p");
 hidden.innerHTML = hiddenWord;
+random.innerHTML = randomWord;
 tempList.appendChild(hidden);
-//=======================================================================================
+tempList.appendChild(random);
 
 
 
 // Get keystroke from user ==============================================================
 document.onkeyup = function(event){
 	wordSearch(event.key);
-	// console.log("User has presseed " + guessLetter);
 }
 //=======================================================================================
 
@@ -63,36 +77,29 @@ document.onkeyup = function(event){
 // Search word for the guessLetter=======================================================
 function wordSearch(guessLetter){
 
-	// var rgxp = new RegExp(guessLetter, "g");
-	// var guessLetterIndex = randomWord.search(rgxp);
 	console.log("The user pressed " + guessLetter);
-	// console.log("The user pressed " + guessLetter + " and that is at the " + guessLetterIndex + " position in the word " + randomWord);
-
-	// var updateWord = hiddenWord.replace(guessLetterIndex, guessLetter);
-	// console.log("The updated word is " + updateWord);
 
 
-	// console.log(randomWord.indexOf(guessLetter));
-
-
-//Loops through the random word and compares each character to the guess character====================
-	for(i=0; i<randomWord.length; i++){
-
-		if (randomWord.charAt(i) === guessLetter){
-			console.log("Position " + i + ") " + guessLetter + " is at position " + i + " in the word " + randomWord);
-
-			// console.log(randomWord.indexOf(guessLetter));
-
-			console.log("UPDATE: " + hiddenWord.replace(hiddenWord.charAt(i), guessLetter));
-		}
-
-		else{
-			console.log("Position " + i + ") " + "'" + guessLetter + "'" + " is NOT at position " + i + " in the word " + randomWord)
+	for (key in wordObject){
+		if (wordObject[key] == guessLetter){
+			console.log(guessLetter + " is at positon " + key);
+			console.log(updateWord.replaceAt(key, guessLetter));
+			updateWord = updateWord.replaceAt(key, guessLetter);
 		}
 	}
-//====================================================================================================
 
-
+	console.log("new hidden word " + updateWord);
 };
 
-//=======================================================================================
+
+
+
+
+
+
+// 	var dumbArray = ['cat', 'dog', 'bird']
+
+// 	dumbArray.forEach(function(val){
+// 		console.log(val);
+// 	});
+// //=======================================================================================
